1c665d064c390404b0d52cb30149963f2038d70d
David Rudder <drig@noses.org>
2013-04-01 14:44:43 -0600

    Initial import from svn

---
+++ app/controllers/Contact.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Contact extends Controller {
+	
+	public static void about() {
+		render();
+	}
+
+	public static void tos() {
+		render();
+	}
+
+	public static void privacy() {
+		render();
+	}
+
+	public static void faq() {
+		render();
+	}
+}
